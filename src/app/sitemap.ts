d90bc7778cbcff9f5a71422bd46cd9f763b8a445
Nikesh Dahal <nikesh.dahal@ebpearls.com>
2026-03-22 17:14:15 +0545

    site map completed

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://tempurl.com",
+      url: "https://toplegaladvisers.com",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://tempurl.com/about-us",
+      url: "https://toplegaladvisers.com/about-us",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://tempurl.com/contact-us",
+      url: "https://toplegaladvisers.com/contact-us",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
